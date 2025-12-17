b9816ca0fed06daf3516fa69dd8023d56febe602
Emon Ahmed <emon0860@gmail.com>
2025-12-17 11:34:36 +0600

    handle review system

--- src/Components/BookDetails.jsx
+++ src/Components/BookDetails.jsx
@@ -1,11 +1,12 @@
 import React, { useRef } from "react";
-import { data, useParams } from "react-router";
+import { useParams } from "react-router";
 import UseAxios from "../Hooks/UseAxios";
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../Hooks/UseAuth";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
 import Swal from "sweetalert2";
+import { FaUserCircle } from "react-icons/fa";
 
 const BookDetails = () => {
   const {
@@ -14,9 +15,13 @@ const BookDetails = () => {
     formState: { errors },
   } = useForm();
 
+  const { register: registerReview, handleSubmit: registerHandleSubmit } =
+    useForm();
+
   const { user, loading } = useAuth();
 
   const bookRef = useRef(null);
+  const reviewRef = useRef(null);
   const instance = UseAxios();
   const { id } = useParams();
 
@@ -24,6 +29,14 @@ const BookDetails = () => {
     queryKey: ["book-details", user?.email],
     queryFn: async () => {
       const res = await instance.get(`/books/${id}`);
+      return res.data;
+    },
+  });
+
+  const { data: reviews = [], refetch } = useQuery({
+    queryKey: ["book-reviews", user?.email],
+    queryFn: async () => {
+      const res = await instance.get(`/reviews/${id}`);
       return res.data;
     },
   });
@@ -58,6 +71,18 @@ const BookDetails = () => {
     bookRef.current.showModal();
   };
 
+  const handleAddReview = (data) => {
+    data.photo = user?.photoURL;
+    data.reviewId = id;
+    instance.post("/review", data).then((res) => {
+      if (res.data.insertedId) {
+        toast.success("Thanks for review");
+        refetch();
+        reviewRef.current.close();
+      }
+    });
+  };
+
   const handleAddOrder = (data) => {
     data.paymentStatus = "unpaid";
     data.status = "pending";
@@ -66,9 +91,10 @@ const BookDetails = () => {
     instance.post("/bookorders", data).then((res) => {
       if (res.data.insertedId) {
         toast.success("Order placed successfully");
+        bookRef.current.close();
+        reviewRef.current.showModal();
       }
     });
-    bookRef.current.close();
   };
 
   const handleWishlist = () => {
@@ -116,11 +142,51 @@ const BookDetails = () => {
           </button>
         </div>
 
-        <dialog
-          id="my_modal_5"
-          ref={bookRef}
-          className="modal modal-bottom sm:modal-middle"
-        >
+        <dialog ref={reviewRef} className="modal modal-bottom sm:modal-middle">
+          <div className="modal-box">
+            <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <h1 className="font-bold text-3xl text-base-content text-center mt-4">
+                  Please Review This Book
+                </h1>
+                <form onSubmit={registerHandleSubmit(handleAddReview)}>
+                  <fieldset className="fieldset">
+                    <label className="label">Your Name</label>
+                    <input
+                      type="text"
+                      className="input"
+                      defaultValue={user?.displayName}
+                      {...registerReview("displayName", { required: true })}
+                    />
+                    <label className="font-semibold text-lg">
+                      select rating
+                    </label>
+                    <select className="select" {...registerReview("rating")}>
+                      <option value="5">⭐⭐⭐⭐⭐</option>
+                      <option value="4">⭐⭐⭐⭐</option>
+                      <option value="3">⭐⭐⭐</option>
+                      <option value="2">⭐⭐</option>
+                      <option value="1">⭐</option>
+                    </select>
+                    <textarea
+                      className="textarea"
+                      placeholder="Write your review..."
+                      {...registerReview("reviewBox", { required: true })}
+                    ></textarea>
+                    <button className="btn btn-neutral mt-4">Add Review</button>
+                  </fieldset>
+                </form>
+              </div>
+            </div>
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn">Close</button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+
+        <dialog ref={bookRef} className="modal modal-bottom sm:modal-middle">
           <div className="modal-box">
             <div className="card bg-base-100 mx-auto w-full max-w-sm shrink-0 shadow-2xl">
               <div className="card-body">
@@ -184,8 +250,8 @@ const BookDetails = () => {
           </div>
         </dialog>
       </div>
-      {/* <h1 className="font-bold text-3xl text-base-content text-center my-8">
-        Customer Reviews For This Service
+      <h1 className="font-bold text-3xl text-base-content text-center my-8">
+        Users Reviews For This Book
       </h1>
       {reviews.length > 0 ? (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
@@ -197,21 +263,21 @@ const BookDetails = () => {
               >
                 <div className="flex items-center gap-4 justify-start w-full">
                   <figure>
-                    {user.photoURL ? (
+                    {review.photo ? (
                       <img
                         src={review.photo}
-                        alt=""
+                        alt="photo"
                         className="w-12 rounded-full"
                       />
                     ) : (
                       <FaUserCircle className="w-12 h-12 rounded-full" />
                     )}
                   </figure>
-                  <h2 className="card-title text-xl">{review.name}</h2>
+                  <h2 className="card-title text-xl">{review.displayName}</h2>
                 </div>
                 <div className="card-body w-full my-2">
                   <p className="font-semibold text-base-content">
-                    {review.review}
+                    {review.reviewBox}
                   </p>
                   <div className="flex items-center justify-end gap-2">
                     {review.rating == 5 ? (
@@ -235,9 +301,9 @@ const BookDetails = () => {
         </div>
       ) : (
         <h1 className="font-bold text-3xl text-base-content text-center my-8">
-          No Reviews In This Service
+          No Reviews In This Book
         </h1>
-      )} */}
+      )}
     </>
   );
 };
