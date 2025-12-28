5b1e1460443e4a25738812cbeffe52ae38ec0bbc
Emon Ahmed <emon0860@gmail.com>
2025-12-28 13:28:13 +0600

    Done Project

--- src/Components/LatestBooks.jsx
+++ src/Components/LatestBooks.jsx
@@ -8,7 +8,11 @@ const LatestBooks = () => {
   const instance = UseAxios();
   const { user } = useAuth();
 
-  const { data: books = [], isPending } = useQuery({
+  const {
+    data: books = [],
+    isPending,
+    isLoading,
+  } = useQuery({
     queryKey: ["latest-books", user?.email],
     queryFn: async () => {
       const res = await instance.get(`/books/latest`);
@@ -16,18 +20,24 @@ const LatestBooks = () => {
     },
   });
 
-  if (isPending) {
-    return <div className="text-center"><span className="loading loading-dots loading-xl"></span></div>;
+  if (isPending || isLoading) {
+    return (
+      <div className="text-center">
+        <span className="loading loading-dots loading-xl"></span>
+      </div>
+    );
   }
   return (
     <>
-      <h1 className="text-4xl font-extrabold text-neutral text-center mb-12">Latest Books</h1>
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-      {books.map((book) => (
-        <EachBook key={book._id} book={book} />
-      ))}
-    </div>
-      </>
+      <h1 className="text-4xl font-extrabold text-neutral text-center mb-12">
+        Latest Books
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 latest-books">
+        {books.map((book) => (
+          <EachBook key={book._id} book={book} />
+        ))}
+      </div>
+    </>
   );
 };
 
